08880bc57ce259352cc6f8a340a9cc1c22f27e59
Alejandro Cordoba <cordoba.alejandro@gmail.com>
2021-09-01 13:33:02 -0300

    Api Lista

--- src/util/ScripTs/Scripts.route.ts
+++ src/util/ScripTs/Scripts.route.ts
@@ -3,14 +3,14 @@ import { validateLogin } from '../../middlewares/passport-jwt';
 
 import { crearJsonApp, crearJsonUsr } from './app';
 
-import { actualizar, actualizarVoto, geoVoto, nuevoVoto, usrConVotos, votosNQN } from './actualizarVotos';
+import { actualizarVoto, geoVoto, nuevoVoto, usrConVotos, votosNQN } from './actualizarVotos';
 
 import { crearCapa } from './padronNuevo';
 
 const router = Router();
 
 router.post('/script/actualizarVoto', actualizarVoto); //Busca la informacion en el array de votos y verifica el idCoordinador, si falta lo agrega
-//router.post('/script/actualizar', actualizar); //Verifica si el el usuario ref tiene el id del Coordinador, si falta lo agrega
+
 router.post('/script/votoadh', crearCapa);
 router.post('/script/listado', usrConVotos);
 router.post('/script/geoEsc', geoVoto);
